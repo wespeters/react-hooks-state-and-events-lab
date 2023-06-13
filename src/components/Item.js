29cb96bd145f330d90e5842f6cb044ed576386fb
Wesley Peters <wesleypeters@gmail.com>
2023-06-13 07:59:04 -0600

    add state and events, pass tests

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  
+  // Use a state variable to track if an item is in the cart
+  const [isInCart, setIsInCart] = useState(false);
+  
+  const handleAddToCart = () => {
+    setIsInCart(!isInCart);
+  };
+
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddToCart} className="add">{isInCart ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
